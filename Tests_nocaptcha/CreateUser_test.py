d091cc2da180a83c1e499be96c21282638705b8b
mwcdproj <archena@billionlives.in>
2018-01-19 14:27:51 -0800

    fix

--- Tests_nocaptcha/CreateUser_test.py
+++ Tests_nocaptcha/CreateUser_test.py
@@ -173,7 +173,7 @@ class login(unittest.TestCase):
         print self.driver.find_element_by_id("Email-error").text
         self.driver.find_element_by_id("Email").clear()
         time.sleep(1)
-        self.driver.find_element_by_id("Email").send_keys("testautomation10@example.com")
+        self.driver.find_element_by_id("Email").send_keys("testautomation101@example.com")
         time.sleep(1)
 
         # ********************** #
@@ -341,14 +341,14 @@ class login(unittest.TestCase):
         flag = 0
         bodyText = self.driver.find_element_by_tag_name('body').text
         if pages == 1:
-            if "testautomation10@example.com" in bodyText:
+            if "testautomation101@example.com" in bodyText:
                 print "User found!!"
                 flag = 1
         else:
             for i in range(1, pages + 1):
                 print "Verifying in page: ", i
                 bodyText = self.driver.find_element_by_tag_name('body').text
-                if "testautomation10@example.com" in bodyText:
+                if "testautomation101@example.com" in bodyText:
                 #if self.driver.find_elements_by_xpath("//*[contains(text(), 'testautomation2@example.com')]"):
                     print "User found!!"
                     flag = 1
