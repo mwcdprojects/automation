7c75d2b23188e24516238dbdf4a0827295e23942
mwcdproj <archena@billionlives.in>
2018-02-26 14:10:37 -0800

    Regrssion => Testcases for First Installment

--- Tests_nocaptcha/Regression/LMP_gt_1_1_2017_and_LMP+150days_gt_1_1_2017_test.py
+++ Tests_nocaptcha/Regression/LMP_gt_1_1_2017_and_LMP+150days_gt_1_1_2017_test.py
@@ -1,7 +1,7 @@
 #!C:\Python27\python.exe
 
 """
-Testcase :When LMP < 1/1/2017 and LMP+1501/1/2017
+Testcase :When LMP < 1/1/2017 and LMP+150 <= 1/1/2017
 Expected Result: Eligible for first instalment.
 
 """
@@ -28,7 +28,7 @@ class login(unittest.TestCase):
         self.desig = ""
         self.contactaddr = ""
 
-    def test_01_Register_Beneficiary(self):
+    def test_01(self):
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
         self.driver.get("http://mwcd1.fundright.in/BackOffice/useraccount/login")
@@ -55,7 +55,7 @@ class login(unittest.TestCase):
         self.driver.find_element_by_id("btnNewbeneficiary").click()
         time.sleep(3)
 
-    def test_02_LMP_lt_1_1_2017_and_LMP_150_lteq_1_1_2017(self):
+    def test_02(self):
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
         self.driver.get("http://mwcd1.fundright.in/BackOffice/useraccount/login")
@@ -105,6 +105,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='BenAlternateIdCheck']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblBenAlternateIdStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblBenAlternateIdStatus']").text,
                         "Id Proof Number is allowed for Registration")
 
@@ -114,6 +115,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='HusbandAlternateIdCheck']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblHusbandAlternateIdStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblHusbandAlternateIdStatus']").text,
                         "Id Proof Number is allowed for Registration")
         self.driver.find_element_by_xpath("//input[@id='NameAsInIDCard']").send_keys("Shaina")
@@ -160,6 +162,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='ifscButton1']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblStatus']").text, "Valid IFSC Code")
         time.sleep(1)
         self.driver.find_element_by_xpath("//input[@id='BankAccountNo']").send_keys("123456789007")
@@ -175,6 +178,7 @@ class login(unittest.TestCase):
         self.driver.switch_to_alert().accept()
         time.sleep(5)
         self.driver.implicitly_wait(20)
+        print self.driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/h5").text
         self.assertTrue(self.driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/h5").text , " The beneficiary application form is sent for approval")
 
     def tearDown(self):
--- Tests_nocaptcha/Regression/Testcase_First_Installment/LMP_gt_1_1_2017_MCP_sub_LMP_lteq_150_test.py
+++ Tests_nocaptcha/Regression/Testcase_First_Installment/LMP_gt_1_1_2017_MCP_sub_LMP_lteq_150_test.py
@@ -28,7 +28,7 @@ class login(unittest.TestCase):
         self.desig = ""
         self.contactaddr = ""
 
-    def test_01_Register_Beneficiary(self):
+    def test_01(self):
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
         self.driver.get("http://mwcd1.fundright.in/BackOffice/useraccount/login")
@@ -55,7 +55,7 @@ class login(unittest.TestCase):
         self.driver.find_element_by_id("btnNewbeneficiary").click()
         time.sleep(3)
 
-    def test_02_LMP_gt_1_1_2017_and_MCP_sub_LMP_lteq_150(self):
+    def test_02(self):
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
         self.driver.get("http://mwcd1.fundright.in/BackOffice/useraccount/login")
@@ -105,6 +105,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='BenAlternateIdCheck']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblBenAlternateIdStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblBenAlternateIdStatus']").text,
                         "Id Proof Number is allowed for Registration")
 
@@ -114,6 +115,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='HusbandAlternateIdCheck']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblHusbandAlternateIdStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblHusbandAlternateIdStatus']").text,
                         "Id Proof Number is allowed for Registration")
         self.driver.find_element_by_xpath("//input[@id='NameAsInIDCard']").send_keys("Arzoo")
@@ -160,6 +162,7 @@ class login(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_xpath("//a[@id='ifscButton1']").click()
         time.sleep(2)
+        print self.driver.find_element_by_xpath("//label[@id='lblStatus']").text
         self.assertTrue(self.driver.find_element_by_xpath("//label[@id='lblStatus']").text, "Valid IFSC Code")
         time.sleep(1)
         self.driver.find_element_by_xpath("//input[@id='BankAccountNo']").send_keys("123453336789007")
@@ -175,6 +178,7 @@ class login(unittest.TestCase):
         self.driver.switch_to_alert().accept()
         time.sleep(5)
         self.driver.implicitly_wait(20)
+        print self.driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/h5").text
         self.assertTrue(self.driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/h5").text , " The beneficiary application form is sent for approval")
 
     def tearDown(self):
